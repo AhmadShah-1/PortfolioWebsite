e5b2c134f70df0bae48428f648ef9d99909fa14a
Syed Ahmad Shah <117304625+AhmadShah-1@users.noreply.github.com>
2022-12-23 22:57:48 -0500

    Add files via upload

---
+++ Site/JavaScript/AnimationSlide_Bars_Text.js
@@ -0,0 +1,109 @@
+window.addEventListener("scroll", function () {
+  var element = document.getElementById("my-element1");
+  var elementPosition = element.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight / 0.4;
+
+  if (elementPosition < screenPosition) {
+    element.classList.remove("hidden");
+    element.classList.add("slide-in1");
+  }
+});
+
+window.addEventListener("scroll", function () {
+  var element = document.getElementById("my-element2");
+  var elementPosition = element.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight / 0.4;
+
+  if (elementPosition < screenPosition) {
+    element.classList.remove("hidden");
+    element.classList.add("slide-in2");
+  }
+});
+
+window.addEventListener("scroll", function () {
+  var element = document.getElementById("my-element3");
+  var elementPosition = element.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight / 0.4;
+
+  if (elementPosition < screenPosition) {
+    element.classList.remove("hidden");
+    element.classList.add("slide-in3");
+  }
+});
+
+window.addEventListener("scroll", function () {
+  var element = document.getElementById("my-element4");
+  var elementPosition = element.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight / 0.4;
+
+  if (elementPosition < screenPosition) {
+    element.classList.remove("hidden");
+    element.classList.add("slide-in4");
+  }
+});
+
+window.addEventListener("scroll", function () {
+  var element = document.getElementById("my-element5");
+  var elementPosition = element.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight / 0.4;
+
+  if (elementPosition < screenPosition) {
+    element.classList.remove("hidden");
+    element.classList.add("slide-in5");
+  }
+});
+
+window.addEventListener("scroll", function () {
+  var element = document.getElementById("my-element6");
+  var elementPosition = element.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight / 0.4;
+
+  if (elementPosition < screenPosition) {
+    element.classList.remove("hidden");
+    element.classList.add("slide-in6");
+  }
+});
+
+window.addEventListener("scroll", function () {
+  var element = document.getElementById("my-element7");
+  var elementPosition = element.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight / 0.4;
+
+  if (elementPosition < screenPosition) {
+    element.classList.remove("hidden");
+    element.classList.add("slide-in7");
+  }
+});
+
+window.addEventListener("scroll", function () {
+  var element = document.getElementById("my-element8");
+  var elementPosition = element.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight / 0.4;
+
+  if (elementPosition < screenPosition) {
+    element.classList.remove("hidden");
+    element.classList.add("slide-in8");
+  }
+});
+
+window.addEventListener("scroll", function () {
+  var element = document.getElementById("my-element9");
+  var elementPosition = element.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight / 0.4;
+
+  if (elementPosition < screenPosition) {
+    element.classList.remove("hidden");
+    element.classList.add("slide-in9");
+  }
+});
+
+window.addEventListener("scroll", function () {
+  var element = document.getElementById("my-element10");
+  var elementPosition = element.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight / 0.4;
+
+  if (elementPosition < screenPosition) {
+    element.classList.remove("hidden");
+    element.classList.add("slide-in10");
+  }
+});
